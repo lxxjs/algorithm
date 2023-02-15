7454fea2703d4409505c02e2b18d2ac41a146929
Jongseo Lee <james0614ll@gmail.com>
2023-02-15 05:48:22 +0000

    Q704

--- Leet_Code/Q704_Binary_Search.py
+++ Leet_Code/Q704_Binary_Search.py
@@ -1,3 +1,7 @@
+# 다중 if문의 순서에 따라 시간 복잡도를 꽤나 낮출 수 있다.
+# nums[mid] == target 3번째 조건은 가장 마지막 한번 혹은 한번도 해당되지 않는 경우도 있기에
+# 마지막에 두는 것이 맞다. 첫번째나 두번째에 두면 답이 false인 논리식을 계속 반복하는 것.
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         left = 0
