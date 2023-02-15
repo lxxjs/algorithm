0de2f821978783ebc5c468d9b1588731111ca85e
Jongseo Lee <james0614ll@gmail.com>
2023-02-15 06:15:14 +0000

    Q278

---
+++ Leet_Code/Binary_Search/Q278_First_Bad_Version.py
@@ -0,0 +1,15 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+# YouDaiTiGao
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left = 1
+        right = n
+        while left < right:
+            mid = (left + right) // 2
+            if isBadVersion(mid) == False:
+                left = mid + 1
+            else:
+                right = mid
+        return left
