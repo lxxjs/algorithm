a9c9766092b80ff63d350697714a163c4492fc15
Jongseo Lee <james0614ll@gmail.com>
2023-02-20 01:23:13 +0000

    Q344

--- Leet_Code/Two_Pointers/Q167_Two_Sum_II_M.py
+++ Leet_Code/Two_Pointers/Q167_Two_Sum_II_M.py
@@ -4,13 +4,11 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         length = len(numbers)
-        ans = []
         left, right = 0, length - 1
         while left < right:
             curr_sum = numbers[left] + numbers[right]
             if curr_sum == target:
-                ans = [left + 1, right + 1]
-                return ans
+                return [left + 1, right + 1]
             elif curr_sum < target:
                 left += 1
             else:
