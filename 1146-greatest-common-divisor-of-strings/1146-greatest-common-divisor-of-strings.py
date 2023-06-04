32be0812f0653989cb1ce30c3282a4a02acfb3c0
Jongseo Lee <james0614ll@gmail.com>
2023-06-04 23:20:44 +0900

    Time: 83 ms (5.01%), Space: 16.3 MB (31.99%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -1,9 +1,13 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        #a = str1.find(str2)
-        
-        #if a == -1:
-        #    return ""
+        if len(str1) >= len(str2):
+            a = str1.find(str2)
+            if a == -1:
+                return ""
+        else:
+            b = str2.find(str1)
+            if b == -1:
+                return ""
         for i in range(min(len(str1), len(str2)), 0, -1):
             if len(str1) >= len(str2):
                 if str1.replace(str2[:i], "") == "" and str2.replace(str2[:i], "") == "":
