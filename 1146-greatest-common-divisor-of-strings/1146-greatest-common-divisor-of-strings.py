81e4c173b7ec2bbe9349f65deedaba63808ba2fe
Jongseo Lee <james0614ll@gmail.com>
2023-06-04 23:31:57 +0900

    Update 1146-greatest-common-divisor-of-strings.py

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -1,5 +1,7 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
+        # My initial solution
+        
         # if str1[0] != str2[0]:
         #     return ""
         # for i in range(min(len(str1), len(str2)), 0, -1):
@@ -10,6 +12,7 @@ class Solution:
         #         if str2.replace(str1[:i], "") == "" and str1.replace(str1[:i], "") == "":
         #             return str1[:i]
 
+        
         if str1 + str2 != str2 + str1:
             return ""
         from math import gcd
