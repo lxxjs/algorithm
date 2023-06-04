e21dbef64ca943d0b3347323eb544d402a8d7f31
Jongseo Lee <james0614ll@gmail.com>
2023-06-04 23:27:31 +0900

    Time: 40 ms (84.77%), Space: 16.4 MB (8.29%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -1,12 +1,16 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        if str1[0] != str2[0]:
+        # if str1[0] != str2[0]:
+        #     return ""
+        # for i in range(min(len(str1), len(str2)), 0, -1):
+        #     if len(str1) >= len(str2):
+        #         if str1.replace(str2[:i], "") == "" and str2.replace(str2[:i], "") == "":
+        #             return str2[:i]
+        #     else:
+        #         if str2.replace(str1[:i], "") == "" and str1.replace(str1[:i], "") == "":
+        #             return str1[:i]
+
+        if str1 + str2 != str2 + str1:
             return ""
-        for i in range(min(len(str1), len(str2)), 0, -1):
-            if len(str1) >= len(str2):
-                if str1.replace(str2[:i], "") == "" and str2.replace(str2[:i], "") == "":
-                    return str2[:i]
-            else:
-                if str2.replace(str1[:i], "") == "" and str1.replace(str1[:i], "") == "":
-                    return str1[:i]
-        return ""
+        from math import gcd
+        return str1[:gcd(len(str1), len(str2))]
