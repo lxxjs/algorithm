07e2034fbf5765773c099863e64ebe42fe97dc9e
Jongseo Lee <james0614ll@gmail.com>
2023-06-04 23:40:23 +0900

    Time: 50 ms (24.57%), Space: 16.3 MB (22.12%) - LeetHub

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.py
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.py
@@ -1,10 +1,10 @@
 class Solution:
     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
         ans = []
+        maxNum = max(candies)
         for i in range(len(candies)):
-            if candies[i] + extraCandies >= max(candies):
+            if candies[i] + extraCandies >= maxNum:
                 ans.append(True)
             else:
                 ans.append(False)
-
         return ans
